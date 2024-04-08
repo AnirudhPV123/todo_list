3e2b0ffa9ec5c52ebdee75fb3f362d5bf9ac0ec8
AnirudhPV123 <pvanirudh96@gmail.com>
2024-04-09 05:26:28 +0530

    frontend debug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://todo-list-r6ks.onrender.com",
-    },
-  },
   plugins: [react()],
-});
+}); 
